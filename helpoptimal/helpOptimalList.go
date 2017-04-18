48a60f4f5a702f2e3771d526ce0b4552bbb81e21
rishdas <sirshakdas@gmail.com>
2017-04-18 12:47:01 -0400

    help optimal go init files

---
+++ helpoptimal/helpOptimalList.go
@@ -0,0 +1,20 @@
+package helpoptimal
+
+
+// import (
+// 	"fmt"
+// 	"unsafe"
+// 	"sync/atomic"
+// )
+
+type key struct {
+	value float64
+	minValue0 float64
+	minValue1 float64
+	maxValue0 float64
+	maxValue1 float64
+	maxValue2 float64
+}
+
+type node struct {
+}
