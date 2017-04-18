3bf6735d74442a8b4decc8d9299e7b26232ded10
rishdas <sirshakdas@gmail.com>
2017-04-18 15:15:02 -0400

    Key API from Concurrent Set

--- helpoptimal/helpOptimalList.go
+++ helpoptimal/helpOptimalList.go
@@ -1,11 +1,13 @@
 package helpoptimal
 
 
-// import (
-// 	"fmt"
-// 	"unsafe"
-// 	"sync/atomic"
-// )
+import (
+	"math"
+	"math/rand"
+	// "fmt"
+	// "unsafe"
+	// "sync/atomic"
+)
 
 type key struct {
 	value float64
@@ -18,3 +20,45 @@ type key struct {
 
 type node struct {
 }
+
+func newKey() *key {
+	k := new(key)
+	k.value = 0
+	k.minValue0 = -math.MaxFloat64
+	k.minValue1 = -0.95*math.MaxFloat64
+	k.maxValue0 = math.MaxFloat64
+	k.maxValue1 = 0.95*math.MaxFloat64
+	k.maxValue2 = 0.9*math.MaxFloat64
+	return k
+}
+func (k *key) compareTo(o *key) bool {
+	return k.value < o.value
+}
+func (k *key) equals(o *key) bool {
+	return k.value == o.value
+}
+func (k *key) getValue() float64 {
+	return k.value
+}
+func (k *key) simpleNew(b *key) *key {
+	if k.value < b.value {
+		return b
+	} else {
+		return k
+	}
+}
+func (k *key) randPoint(b *key) *key {
+	nk := new (key)
+	if k.value < b.value {
+		nk.value = (rand.Float64()*k.value) + b.value		
+	} else {
+		nk.value = (rand.Float64()*b.value) + k.value
+	}
+	return nk
+}
+func (k *key) newPoint(b *key) *key {
+	nk := new (key)
+	nk.value = k.value*0.5 + b.value*0.5
+	return nk
+}
+
