6512a9930e0756736c21a46070a25f10c879316e
rishdas <sirshakdas@gmail.com>
2017-04-18 22:01:59 -0400

    helpOptimalLFList add and contains

--- helpoptimal/helpOptimalList.go
+++ helpoptimal/helpOptimalList.go
@@ -18,6 +18,60 @@ func newHelpOptimalLFList() *helpOptimalLFList {
 	return hoLFList
 }
 
+func (hoLFList *helpOptimalLFList) getRef(n *node) *node{
+	if n.back == nil {
+		return n
+	} else {
+		return n.next
+	}
+}
+func (hoLFList *helpOptimalLFList) getNext(n *node) *node{
+	return hoLFList.getRef(n.next)
+}
 
+func (hoLFList *helpOptimalLFList) contains(k *key) bool{
+	cur := hoLFList.getNext(hoLFList.headNext);
+	for cur.key.compareTo(k) == true {
+		cur = cur.next
+	}
+	return k.equals(cur.key) && cur.next.back == nil;
+}
 
-
+func (hoLFList *helpOptimalLFList) add(k *key) bool{
+	pre := hoLFList.head
+	suc := hoLFList.headNext
+	cur := hoLFList.headNext
+	nex := cur.next
+	for true {
+		for cur.key.compareTo(k) == true {
+			if nex.back == nil {
+				pre = cur
+				suc = nex
+				cur = nex
+			} else {
+				cur = nex.next
+			}
+			nex = cur.next
+		}
+		if (nex.back != nil) {
+			for nex.back != nil {
+				cur = nex.next
+				nex = cur.next
+			}
+		} else if cur.key.equals(k) == true {
+			return false
+		}
+		if pre.casNext(suc, newNodeNext(k, cur)) == true {
+			return true
+		}
+		suc = pre.next
+		for suc.back != nil {
+			pre = suc.back
+			suc = pre.next
+		}
+		cur = pre
+		nex = suc
+	}
+	//Dead Code
+	return false
+}
