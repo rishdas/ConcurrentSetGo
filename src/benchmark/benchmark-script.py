f11d3b958eb0974d4e92df2d012087291ed20456
sirdas <sirdas@indiana.edu>
2017-04-27 01:07:47 -0400

    Benchmark script averaging not done

---
+++ src/benchmark/benchmark-script.py
@@ -0,0 +1,29 @@
+from subprocess import Popen, PIPE
+import os
+
+sum_e = 0
+num_of_threads = 2
+data_sizes = ['128', '512', '1024', '4096']
+binary = './benchmark'
+algo_l = ['HelpOptimalLFList', 'HarrisLinkedList']
+percentages = [('50', '50', '0'), ('20', '10', '70'), ('9', '1', '90')]
+duration = '5'
+for data_size in data_sizes:
+    for add_p, rem_p, con_p in percentages:
+        for algo in algo_l:
+            while num_of_threads <= 64:
+                for num in range(0,6):
+                    algo_a = '-a='+algo
+                    dur_a = '-d='+duration
+                    add_a = '-i='+add_p
+                    k_a = '-k='+data_size
+                    n_a = '-n='+str(num_of_threads)
+                    con_a = '-r='+con_p
+                    rem_a = '-x='+rem_p
+                    p = Popen([binary, algo_a, dur_a, add_a, k_a, n_a, con_a, rem_a], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+                    out, err = p.communicate()
+                    exitcode = p.returncode
+                    print out
+                num_of_threads *= 2
+                sum_e = 0
+            num_of_threads = 2
